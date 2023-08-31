91f0cd5fbb663b3a129f701dd110384eeafc405a
DaniloDiaz98 <eddiaz98@hotmail.com>
2023-08-30 20:46:24 -0500

    act tit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <>
       <h1 className="text-center font-bold text-4xl mb-20 mt-5">
-        Seguimiento de Mascotas
+        Citas de Mascotas
       </h1>
     </>
   );
